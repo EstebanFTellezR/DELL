b5889e84c423f64389ce5b997ffdefd2ff8fa666
EstebanFTellezR <tellezruizestebanfelipe@gmail.com>
2023-08-15 15:46:01 -0500

    Se agrego dos case

--- Calculadora.py
+++ Calculadora.py
@@ -3,7 +3,7 @@ print("Ingrese el número 1")
 numero1 = int(input())
 print("Ingrese el número 2")
 numero2 = int(input())
-print("Ingrese la operación (+, -, *, /)")
+print("Ingrese la operación (+, -, *, /, %, **)")
 operacion = input()
 
 match operacion:
@@ -23,5 +23,13 @@ match operacion:
         # Operación división:
         division = numero1 / numero2
         print("La división es " + str(division))
+    case '%':
+        #Operación Modulo:
+        modulo = numero1 % numero2
+        print("El modulo es " + str(modulo))
+    case '**':
+        # Operación Potencia:
+        potencia = numero1 ** numero2
+        print("La potencia es " + str(potencia))
     case _ :
         print("Operación inválida")
